5b23b385cd617b958907bfbe0e01592ef400ae5b
zezo1895 <143358105+zezo1895@users.noreply.github.com>
2024-12-26 01:37:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import logo from './logo.svg';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import { createBrowserRouter, createHashRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Root from './pages/root';
 import Home from './pages/home';
 import { useRef } from 'react';
+
 function App() {
-  const sectionRefs = [
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-  ];
-  const scrollToSection = (index) => {
-    sectionRefs[index].current.scrollIntoView({ behavior: 'smooth' });
-  };
-  const router = createHashRouter(
+  const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />} >
-      <Route index element={<Home/>}/>
-
-  
+        <Route index element={<Home />} />
       </Route>
-    )
+    ),
+    {
+      basename: "/MFIPCHEM"  // هنا نضع basename مع اسم مستودع GitHub
+    }
   );
 
-
   return (
-<RouterProvider router={router} />
+    <RouterProvider router={router} />
   );
 }
 
